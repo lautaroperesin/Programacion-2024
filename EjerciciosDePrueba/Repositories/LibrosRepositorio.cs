fb9ed22d01f0cd4ca4c5f7a6b9a48cdd680f9345
Lautaro <lautaroperesin@gmail.com>
2024-06-04 13:23:57 -0300

    Tests Libros

--- EjerciciosDePrueba/Repositories/LibrosRepositorio.cs
+++ EjerciciosDePrueba/Repositories/LibrosRepositorio.cs
@@ -21,10 +21,76 @@ namespace EjerciciosDePrueba.Repositories
             client.DefaultRequestHeaders.Add("apikey", "64f8c70e68885478480bfe86");
         }
 
-        public async Task<ObservableCollection<Libro>> ObtenerLibrosAsync()
+        public async Task<ObservableCollection<Libro>?> ObtenerLibrosAsync()
         {
             var response = await client.GetStringAsync(urlApi);
             return JsonConvert.DeserializeObject<ObservableCollection<Libro>>(response);
+        }
+
+        public async Task<Libro?> AgregarAsync(string nombre, int paginas, string autor, string editorial, string genero, string sinopsis, string portada_url)
+        {
+            // Creamos un objeto del tipo Libro con los parámetros que llegan
+            Libro libro = new Libro()
+            {
+                nombre = nombre,
+                paginas = paginas,
+                autor = autor,
+                editorial = editorial,
+                genero = genero,
+                sinopsis = sinopsis,
+                portada_url = portada_url
+            };
+
+            // Enviamos por POST el objeto que creamos a la URL de la API
+            var libroJson = new StringContent(
+                    JsonConvert.SerializeObject(libro),
+                    Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(urlApi,
+                new StringContent(
+                    JsonConvert.SerializeObject(libro),
+                    Encoding.UTF8, "application/json"));
+
+            // Retorna el objeto que se agregó en la API ya con su ID generado por la base de datos
+            return JsonConvert.DeserializeObject<Libro>(
+                await response.Content.ReadAsStringAsync());
+        }
+
+        public async Task<Libro?> ActualizarAsync(string nombre, int paginas, string autor, string editorial, string genero, string sinopsis, string portada_url, string id)
+        {
+            //creamos un objeto del tipo Libro con los parámetros que llegan
+            Libro libro = new Libro()
+            {
+                nombre = nombre,
+                paginas = paginas,
+                autor = autor,
+                editorial = editorial,
+                genero = genero,
+                sinopsis = sinopsis,
+                portada_url = portada_url
+            };
+
+            //enviamos por PUT el objeto que creamos a la URL de la API
+            var librojson = new StringContent(
+                    JsonConvert.SerializeObject(libro),
+                    Encoding.UTF8, "application/json");
+
+            var response = await client.PutAsync(urlApi + "/" + id, librojson);
+
+            //retorna el objeto que se agregó en la API ya con su ID generado por la base de datos
+            return JsonConvert.DeserializeObject<Libro>(
+                await response.Content.ReadAsStringAsync());
+        }
+
+        public async Task<Libro?> ObtenerPorIdAsync(string id)
+        {
+            var response = await client.GetStringAsync($"{urlApi}/{id}");
+            return JsonConvert.DeserializeObject<Libro>(response);
+        }
+
+        public async Task<bool> EliminarAsync(string id)
+        {
+            var response = await client.DeleteAsync(urlApi + "/" + id);
+            return response.IsSuccessStatusCode;
         }
     }
 }
--- ProyectoTesteoLibros/TestLibros.cs
+++ ProyectoTesteoLibros/TestLibros.cs
@@ -14,7 +14,39 @@ namespace ProyectoTesteoLibros
         {
             LibrosRepositorio librosRepositorio = new LibrosRepositorio();
             var libros = await librosRepositorio.ObtenerLibrosAsync();
-            Assert.Pass();
+            Assert.That(libros.Count, Is.Not.EqualTo(0));
+        }
+
+        [Test]
+        public async Task TestAgregarLibro()
+        {
+            LibrosRepositorio librosRepositorio = new LibrosRepositorio();
+            var libro = await librosRepositorio.AgregarAsync("LibroText", 20, "AutorText", "EditorialText", "GeneroText", "SinopsisText", "PortadaText");
+            Assert.That(libro.nombre, Is.EqualTo("LibroText"));
+        }
+
+        [Test]
+        public async Task TestEliminarLibro()
+        {
+            LibrosRepositorio librosRepositorio = new LibrosRepositorio();
+            var borrado = await librosRepositorio.EliminarAsync("665a14dcb384736b000011f1");
+            Assert.That(borrado, Is.EqualTo(true));
+        }
+
+        [Test]
+        public async Task TestObtenerLibro()
+        {
+            LibrosRepositorio librosRepositorio = new LibrosRepositorio();
+            var libro = await librosRepositorio.ObtenerPorIdAsync("653860edb4422a5c00000013");
+            Assert.That(libro.nombre, Is.EqualTo("Nueva Comedia Americana"));
+        }
+
+        [Test]
+        public async Task TestActualizarLibro()
+        {
+            LibrosRepositorio librosRepositorio = new LibrosRepositorio();
+            var libro = await librosRepositorio.ActualizarAsync("LibroActualizado", 20, "AutorText", "EditorialText", "GeneroText", "SinopsisText", "PortadaText", "65147e48c3119e6f000001f6");
+            Assert.That(libro.nombre, Is.EqualTo("LibroActualizado"));
         }
     }
 }
